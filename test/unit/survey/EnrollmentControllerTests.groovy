1ba35ff2f884210487c8107f43877672ed66a510
joh04926 <joh04926@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-03-31 19:55:50 +0000

    Added some tests that pass, nuff said.
    git-svn-id: http://cscirepo.morris.umn.edu/svn/csci-3601/Spring12/ReleaseThree/CevinsCyqulists@9195 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- test/unit/survey/EnrollmentControllerTests.groovy
+++ test/unit/survey/EnrollmentControllerTests.groovy
@@ -29,13 +29,62 @@ class EnrollmentControllerTests extends ControllerUnitTestCase {
 	
 	controller.params.course = course1
 	def results = controller.create()
-	
-	assertNotNull results
-		
+	assertNotNull results	
 	assertTrue results.availableStudents.containsAll([person2, person3,owner])
-	//assertEquals 1 , results.unavailableStudents.size()
 	assertEquals 3, results.availableStudents.size()
 	
 
+    }
+    void testSaveStudents() {  
+        
+        Person owner = new Person(name: "Kate", email: "sup@sup.com")
+        mockDomain(Person, [owner])
+        Course course1 = new Course(owner: owner, name: "course1")      
+        mockDomain(Course, [course1])
+        Enrollment enrollment = new Enrollment(course:course1, person: owner)
+        mockDomain(Enrollment,[])
+        
+        controller.params.course = course1
+        controller.params.name = owner.name
+        def results = controller.save()
+        assertNull results
+        assertEquals controller.redirectArgs.action, "create"
+        assertEquals controller.redirectArgs.controller, "enrollment"
+        assertEquals controller.redirectArgs.params, ['course.id': course1.id]
+        
+    }
+    void testInvalidEnrollment() {
+        Person owner = new Person(name: "Kate", email: "sup@sup.com")
+        mockDomain(Person, [owner])
+        Course course1 = new Course(owner: owner, name: "course1")
+        mockDomain(Course, [course1])
+        mockDomain(Enrollment,[])
+        controller.params.course = course1
+        controller.params.name = null
+        def results = controller.save()
+        assertEquals results.Student, null
+        assertEquals controller.renderArgs.view, "create"
+        
+    }
+    void testShowEnrollment() {
+        Person owner = new Person(name: "Kate", email: "sup@sup.com")
+        mockDomain(Person, [owner])
+        Course course1 = new Course(owner: owner, name: "course1")
+        mockDomain(Course, [course1])
+        Enrollment enrollment = new Enrollment(course:course1, person: owner)
+        mockDomain(Enrollment,[enrollment])
+         controller.params.id = owner.id
+         def results = controller.show()
+         assertEquals results.enrollmentInstance, enrollment
+    }
+    void testShowInvalidEnrollment(){
+        Person owner = new Person(name: "Kate", email: "sup@sup.com")
+        mockDomain(Person, [owner])
+        Course course1 = new Course(owner: owner, name: "course1")
+        mockDomain(Course, [course1])
+        Enrollment enrollment = new Enrollment(course:course1, person: owner)
+        mockDomain(Enrollment,[enrollment])
+        controller.params.id = null
+        assertEquals controller.request.message, null
     }
 }

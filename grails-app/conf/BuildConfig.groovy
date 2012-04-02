d9ae0242101f01fe7229a421e89915341d0c67a4
perra044 <perra044@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-04-02 00:31:45 +0000

    Trying to get spock working again...... derp
    git-svn-id: http://cscirepo.morris.umn.edu/svn/csci-3601/Spring12/ReleaseThree/CevinsCyqulists@9227 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -18,7 +18,7 @@ grails.project.dependency.resolution = {
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
         //mavenLocal()
-        //mavenCentral()
+        mavenCentral()
         //mavenRepo 'http://snapshots.repository.codehaus.org'
         //mavenRepo 'http://repository.codehaus.org'
         //mavenRepo 'http://download.java.net/maven/2/'
@@ -28,5 +28,12 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.13'
+		
+		test "org.spockframework:spock-grails-support:0.6-groovy-1.7"
+  }
+  plugins {
+    test(":spock:0.6") {
+      exclude "spock-grails-support"
     }
+  }
 }
--- test/unit/survey/PersonSpec.groovy
+++ test/unit/survey/PersonSpec.groovy
@@ -1,28 +1,28 @@
 package survey
 
-//import grails.plugin.spock.*
-//
-//class PersonSpec extends UnitSpec {
-//	Person matt = new Person(name: 'Matt', email: 'cats@gmail.com')
-//	
-//	def setup() {
-//		mockForConstraintsTests(Person)
-//	}
-//	
-//	def 'Test Name and Email'() {
-//		expect:
-//			matt.name == 'Matt'
-//			matt.email == 'cats@gmail.com'
-//	}
-//	
-//	def 'Test toString'() {
-//		expect:
-//			matt.toString() == 'Matt'
-//	}
-//	
-//	def 'Test User Validate'() {
-//		when: mockDomain(Person, [matt])
-//
-//		then: matt.validate() == true
-//	}
-//}
+import grails.plugin.spock.*
+
+class PersonSpec extends UnitSpec {
+	Person matt = new Person(name: 'Matt', email: 'cats@gmail.com')
+	
+	def setup() {
+		mockForConstraintsTests(Person)
+	}
+	
+	def 'Test Name and Email'() {
+		expect:
+			matt.name == 'Matt'
+			matt.email == 'cats@gmail.com'
+	}
+	
+	def 'Test toString'() {
+		expect:
+			matt.toString() == 'Matt'
+	}
+	
+	def 'Test User Validate'() {
+		when: mockDomain(Person, [matt])
+
+		then: matt.validate() == true
+	}
+}

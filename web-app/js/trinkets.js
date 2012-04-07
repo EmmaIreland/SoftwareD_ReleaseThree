fcd8cc8715d279957bf20d0284195b17e8b71452
virat001 <virat001@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-04-07 20:58:06 +0000

    Fixed problem where clicking on the inside of an expanded collapsibleDiv triggered collapse
    git-svn-id: http://cscirepo.morris.umn.edu/svn/csci-3601/Spring12/ReleaseThree/CevinsCyqulists@9420 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- web-app/js/trinkets.js
+++ web-app/js/trinkets.js
@@ -1,13 +1,13 @@
 function toggleCollapse(event) {
-	if ( $(this).attr("class") == "collapsibleDiv collapsed" ) {
-		$(this).attr("class", "collapsibleDiv expanded");
+	if ( $(this).parent().attr("class") == "collapsibleDiv collapsed" ) {
+		$(this).parent().attr("class", "collapsibleDiv expanded");
 	} else {
-		$(this).attr("class", "collapsibleDiv collapsed");
+		$(this).parent().attr("class", "collapsibleDiv collapsed");
 	}
 }
 
 function setupTrinkets() {
-	$(".collapsibleDiv").bind({ "click": toggleCollapse });
+	$(".collapsibleDiv .header").bind({ "click": toggleCollapse });
 }
 
 $(function() {

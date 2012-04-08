1670b4a52741de3ce7a02575a033678be8cb139c
risl0014 <risl0014@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-04-08 19:10:13 +0000

    Started implementing Authentication (hacking our own)
    git-svn-id: http://cscirepo.morris.umn.edu/svn/csci-3601/Spring12/ReleaseThree/CevinsCyqulists@9431 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- grails-app/domain/survey/Person.groovy
+++ grails-app/domain/survey/Person.groovy
@@ -3,11 +3,14 @@ package survey
 class Person {
     String name
     String email
+    String password
+    Boolean isAdmin = false
     static hasMany = [ownedCourses:Course, enrollments:Enrollment, memberships: Membership, answers: Answer]
     
     static constraints = {
         name(blank: false)
         email(email: true, blank:false, unique: true)
+	password(blank: false, size:5..15)
     }
     
     String toString() {
--- grails-app/services/survey/AuthenticationService.groovy
+++ grails-app/services/survey/AuthenticationService.groovy
@@ -7,4 +7,22 @@ class AuthenticationService {
     def serviceMethod() {
 
     }
+    
+    def validateLogin(String email, String password) {
+	def person = Person.findByEmail(email)
+	( person && person.password == hashPassword(password) ) ? person : null
+    }
+    
+    def loginPerson(Person person) {
+	session['user'] = person.id
+    }
+    
+    def logout() {
+	session.invalidate()
+    }
+    
+    // TODO Use a real hash algorithm
+    def hashPassword(String password) {
+    	password + '*'
+    }	
 }

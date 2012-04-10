ad1349e5afff5a35fb467cae74c48ddb9378ca48
perra044 <perra044@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-04-10 00:13:11 +0000

    Tried adding the dependencies and plugins into the build config
    git-svn-id: http://cscirepo.morris.umn.edu/svn/csci-3601/Spring12/ReleaseThree/CevinsCyqulists@9471 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -24,16 +24,31 @@ grails.project.dependency.resolution = {
         //mavenRepo 'http://download.java.net/maven/2/'
         //mavenRepo 'http://repository.jboss.com/maven2/'
     }
+	
+	def gebVersion = "0.6.0"
+	def seleniumVersion = "2.0rc3"
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.13'
 		
 		test "org.spockframework:spock-grails-support:0.6-groovy-1.7"
+		
+		test("org.seleniumhq.selenium:selenium-htmlunit-driver:$seleniumVersion") {
+			exclude "xml-apis"
+		}
+		test("org.seleniumhq.selenium:selenium-chrome-driver:$seleniumVersion")
+		test("org.seleniumhq.selenium:selenium-firefox-driver:$seleniumVersion")
+
+		// You usually only need one of these, but this project uses both
+		test "org.codehaus.geb:geb-spock:$gebVersion"
+		test "org.codehaus.geb:geb-junit4:$gebVersion"
   }
   plugins {
-    test(":spock:0.6") {
-      exclude "spock-grails-support"
-    }
+	  	test ":tomcat:$grailsVersion"
+        test ":hibernate:$grailsVersion"
+
+        test ":geb:$gebVersion"
+        test ":spock:0.5-groovy-1.7"
   }
 }

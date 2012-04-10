a995931634984a83c66ad43b712eb893cf59329f
joh04926 <joh04926@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-04-10 02:11:16 +0000

    Wrote two taglib tests pewpew
    git-svn-id: http://cscirepo.morris.umn.edu/svn/csci-3601/Spring12/ReleaseThree/CevinsCyqulists@9477 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- test/unit/survey/TrinketsTagLibTests.groovy
+++ test/unit/survey/TrinketsTagLibTests.groovy
@@ -3,15 +3,33 @@ package survey
 import grails.test.*
 
 class TrinketsTagLibTests extends TagLibUnitTestCase {
+    StringWriter out
     protected void setUp() {
         super.setUp()
+        out = new StringWriter()
+        TrinketsTagLib.metaClass.out = out
+        
     }
 
     protected void tearDown() {
         super.tearDown()
+        def remove = GroovySystem.metaClassRegistry.&removeMetaClass
+        remove TrinketsTagLib
     }
 
-    void testSomething() {
-
+    void testCollapsibleDiv() {
+        TrinketsTagLib ttl = new TrinketsTagLib()
+        
+        def results = ttl.collapsibleDiv([title:'title', collapsed:'true'], {""})
+        println results.class
+        assertEquals results, ttl.out
+    }
+    void testIfNullBlank() {
+        TrinketsTagLib ttl = new TrinketsTagLib()
+        def results = ttl.ifNullBlank('thing')
+        assertEquals results, 'thing'
+        results = ttl.ifNullBlank(null)
+        assertEquals results, ''
+       
     }
 }
